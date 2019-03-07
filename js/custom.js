30aefb6df8f22445f54e1570967237e896f628b0
Juan Rios <juandarr@gmail.com>
2019-03-07 01:13:32 -0500

    test: new js function definition to achieve desired effect in cards.
    Former-commit-id: a8d571dad54b41c875457890f2942ea3303c5386
    Former-commit-id: b5c6f29b2b670cb452955998e956684d45f63954

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,17 @@
+var x = document.getElementsByClassName("cardjig");
+var i;
+for (i = 0; i < x.length; i++) {
+  x[i].addEventListener("mouseover", jiggle);
+} 
+
+function jiggle() {
+	var x = document.getElementsByClassName("cardjig");
+    var i;
+    for (i = 0; i < x.length; i++) {
+      x[i].transition('jiggle');
+    } 
+}
+/*
 object.addEventListener("mouseover", jiggle);
 object.addEventListener("mouseover", tada);
 
@@ -10,4 +24,4 @@ function tada() {
     $('.p-icon')
   .transition('tada');
 }
-
+*/
