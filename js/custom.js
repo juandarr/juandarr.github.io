e6c3115436841486adfdbf655ad3431ab7ac8438
Juan Rios <juandarr@gmail.com>
2019-03-07 01:49:17 -0500

    fix: color transition logic in custom js.

--- js/custom.js
+++ js/custom.js
@@ -16,16 +16,17 @@ function jiggle() {
 $(document).ready(function() {
 
     var colors = ['coral', 'cornflowerblue', 'crimson','deepskyblue','gold','darkseagreen'];
-    var random_color = colors[Math.floor(Math.random() * colors.length)];
+    var random_color;
     var x = document.getElementsByClassName('cardjig');
     var i;
     for (i = 0; i < x.length; i++) {
+        random_color = colors[Math.floor(Math.random() * colors.length)];
         x[i].style.background = random_color;
     }  
 
     $('.cardjig').hover(function(){
         $(this).transition({
-            animation  : 'tada',
+            animation  : 'jiggle',
             duration   : '1s'
             });
     }, function(){});
