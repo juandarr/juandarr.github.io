fa95644ebc566ad39f58e018f82606e150846312
Juan Rios <juandarr@gmail.com>
2019-03-07 01:46:31 -0500

    add: change of background color for cards in blog section.
    Former-commit-id: 788db1d7269c70143e2d96b18647fc3cfd6e7029
    Former-commit-id: ab4120a3e5499fc5e175f1b4948a6fafa6569894

--- js/custom.js
+++ js/custom.js
@@ -14,8 +14,20 @@ function jiggle() {
 }*/
 
 $(document).ready(function() {
+
+    var colors = ['coral', 'cornflowerblue', 'crimson','deepskyblue','gold','darkseagreen'];
+    var random_color = colors[Math.floor(Math.random() * colors.length)];
+    var x = document.getElementsByClassName('cardjig');
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.background = random_color;
+    }  
+
     $('.cardjig').hover(function(){
-        $(this).transition('jiggle');
+        $(this).transition({
+            animation  : 'tada',
+            duration   : '1s'
+            });
     }, function(){});
 });
 
